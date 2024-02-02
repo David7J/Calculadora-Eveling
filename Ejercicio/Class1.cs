ac3f87fc956671e80496be3cba168a53fa8d9795
David Jeshua Áreas Pérez <142626297+David7J@users.noreply.github.com>
2024-02-02 11:29:56 -0600

    Add files via upload

---
+++ Ejercicio/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using ConsoleApp3;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    public class Class1
+    {
+        public static void Main(String[] args)
+        {
+            Calcular g = new Calcular();
+            double n1 = new double();
+            double n2 = new double();
+            double resul1, resul2, resul3, resul4;
+            int opc = 0;
+                
+            do 
+            {
+                n1 = g.validar("Digite el primer numero: ");
+
+            }while(n1 <= 0 || n1 > 1000000000);
+            g.setA(n1);
+
+            do
+            {
+                n2 = g.validar("Digite el segundo numero: ");
+
+            } while (n2 <= 0 || n2 > 1000000000);
+            g.setB(n2);
+
+            resul1 = g.getnum(n1, n2);
+
+            resul2 = g.getnum1(n2, n1);
+
+            resul3 = g.getnum2(n1, n2);
+
+            resul4 = g.getnum3(n1, n2);
+
+            Console.WriteLine("Selecccione la operacion que desea realizar: ");
+            Console.WriteLine("1-Suma ");
+            Console.WriteLine("2-Resta ");
+            Console.WriteLine("3-Multiplicacion");
+            Console.WriteLine("4-Division");
+            do
+            {
+                opc = g.validar("Seleccione");
+            } while (opc <= 0 || opc > 4);
+
+
+            switch (opc)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("El resultado de la suma es: ");
+                        Console.WriteLine(resul1);
+                break;
+                    }
+
+                case 2:
+                    {
+                        Console.WriteLine("El resultado de la resta es: ");
+                        Console.WriteLine(resul2);
+                break;
+                    }
+
+                case 3:
+                    {
+                        Console.WriteLine("El resultado de la multiplicacion es: ");
+                        Console.WriteLine(resul3);
+                break;
+                    }
+
+                case 4:
+                    {
+                        Console.WriteLine("El resultado de la division es: ");
+                        Console.WriteLine(resul4);
+                        break;
+                    }
+            }
+        }
+
+
+    }
+}
---
+++ Ejercicio/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Calcular
+    {
+        double a, b;
+
+        public Calcular()
+        {
+
+        }
+
+        public Calcular(double a)
+        {
+            this.a = a;
+        }
+
+        public void setA(double a)
+        {
+            this.a=a;
+        }
+
+        public void setB(double b)
+        {
+            this.b=b;   
+        }
+
+        public double getnum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double getnum1(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double getnum2(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double getnum3(double a, double b)
+        {
+            return a / b;           
+        }
+
+        public int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.WriteLine(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("No se aceptan letras: ", error.Message);
+                }
+            } while (v == 0);
+            return num;
+        }
+
+    }
+
+}
